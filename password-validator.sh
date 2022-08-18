3c7fa51fb90142282159aeff4e19da050e6bb89d
mayurmangire <73816549+mayurmangire@users.noreply.github.com>
2022-08-18 20:04:39 +0530

    Add files via upload

---
+++ password-validator.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#Defining color codes
+
+success='\033[0;32m'
+warning='\033[0;31m'
+
+# Defining password length and input variable
+
+
+len_password=10
+
+pswd_input=$1
+
+password_conditions () {       
+
+if (( ${#pswd_input} < $len_password )); then
+    echo -e "${warning} Please enter a password which is 10 char long"
+    exit 1 ;
+
+elif [[ $pswd_input != *[[:digit:]]* ]]; then
+    echo -e "${warning} Please enter atleast one digit"
+    exit 1;
+
+elif [[ $pswd_input != *[[:lower:]]* ]]; then
+    echo -e "${warning} Please enter atleast one lower case letter"
+    exit 1;
+
+
+elif [[ $pswd_input != *[[:upper:]]* ]]; then
+    echo -e "${warning} Please enter atleaset one upper case letter"
+    exit 1;
+
+fi
+
+}
+
+
+while getopts ":f:" OPTION; do
+	case $OPTION in
+		f)
+		    FILE_PATH=$OPTARG
+			;;
+	esac
+done
+
+
+if [ ! -z "$FILE_PATH" ]
+then
+		pswd_input=$(<$FILE_PATH)
+fi
+
+
+password_conditions $pswd_input
+
+echo -e "${success} Your password is validated"
+
+exit 0
+ 
